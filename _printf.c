c7dd9526886561380b098e8f1e5fc3956bf61814
Ewairim <kihumbaeunice10@gmail.com>
2023-08-22 00:05:54 -0400

    printf file

--- _printf.c
+++ _printf.c
@@ -32,7 +32,7 @@ int _printf(const char *format, ...)
 			{
 				c = va_arg(list_format, int);
 				write(1, &c, 1);
-				count += count;
+				count ++;
 			}
 			else if (*format == 's')
 			{
@@ -42,7 +42,8 @@ int _printf(const char *format, ...)
 			}
 			else if (*format == '%')
 			{
-				count += write(1, format, 1);
+				write(1, format, 1);
+				count++;
 			}
 		}
 		format++;
