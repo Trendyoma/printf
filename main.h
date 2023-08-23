59bfd7197f6f49c32e35734495c7a81643b27879
chioma <chukwudichiomac@gmail.com>
2023-08-22 18:29:48 -0700

    main.h file

--- main.h
+++ main.h
@@ -4,9 +4,23 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <limits.h>
 #include <stdio.h>
 #include <string.h>
 int _printf(const char *format, ...);
-int my_put(char a);
+int my_putch(char c);
 void string_lit(char *str);
+int my_put(va_list list_format);
+int my_string(va_list list_format);
+/**
+ * struct type - for specifiers
+ * @va_list: contains parameter
+ * @f: funtction pointer to specifiers
+ *
+ */
+typedef struct type
+{
+	char *spec;
+	int (*f)(va_list);
+}sp;
 #endif
